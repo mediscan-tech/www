f686648503bae9daeaef2368b2d3aaf7231d66ae
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-09-04 10:02:34 -0500

    remove async function for now

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
+    .middleware(({ }) => {
       // This code runs on your server before upload
  
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
