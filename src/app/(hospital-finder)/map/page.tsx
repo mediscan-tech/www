136f990c4a704694502b33b64278ee31295bff85
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-09-17 11:25:30 -0500

    add new routes

---
+++ src/app/(hospital-finder)/map/page.tsx
@@ -0,0 +1,7 @@
+export default function MapPage() {
+  return (
+    <>
+      <h1>Map Page Goes Here</h1>
+    </>
+  );
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Landing Page</h1>
+        </div>
+      </main>
+    </>
+  );
+}
